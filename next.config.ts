84503db58b9553370e65a98e77fb6e5a6f9d07ed
Jungsun Lee <solowosophia13@gmail.com>
2025-05-10 18:05:42 +0900

    edit next config

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,12 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // experimental: {
-  //   serverComponentsExternalPackages: ['@prisma/client', 'bcrypt']
-  // }
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcrypt']
+  },
+  env: {
+    COOKIE_PASSWORD: process.env.COOKIE_PASSWORD
+  }
 }
  
 export default nextConfig
